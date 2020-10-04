f096c59facf139217e7bb778b66639d9bba7a38a
Muhammad Umar Farooq <umar@orderspot.co>
2020-10-04 12:31:41 +0500

    ScrollView

--- screens/SearchScreen.jsx
+++ screens/SearchScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import SearchInput from "../components/SearchInput";
 import useResults from "../Hooks/useResults";
 import ResultList from "../components/ResultList";
@@ -14,20 +14,19 @@ const SearchScreen = () => {
   };
   
   return (
-    <View>
+    <>
       <SearchInput
         onTermChange={setTerm}
         onTermSubmit={() => onTermSubmit(term)}
         term={term}
       />
       {error ? <Text>{error}</Text> : null}
-      <Text>You have {results.length} results.</Text>
-      
-      <ResultList results={filterResultsByPrice("$")} title="Cost Effective"/>
-      <ResultList results={filterResultsByPrice("$$")} title="Bit Pricier"/>
-      <ResultList results={filterResultsByPrice("$$$")} title="Big Spender"/>
-    
-    </View>
+      <ScrollView>
+        <ResultList results={filterResultsByPrice("$")} title="Cost Effective"/>
+        <ResultList results={filterResultsByPrice("$$")} title="Bit Pricier"/>
+        <ResultList results={filterResultsByPrice("$$$")} title="Big Spender"/>
+      </ScrollView>
+    </>
   );
 };
 
