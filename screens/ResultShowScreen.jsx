4b64a9931d48d6d08d0d62b8404e196bb1bcf51c
Muhammad Umar Farooq <umar@orderspot.co>
2020-10-04 15:12:39 +0500

    fetching a single restaurant

--- screens/ResultShowScreen.jsx
+++ screens/ResultShowScreen.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import yelp from "../api/yelp";
 
 const ResultShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam("id");
-  console.log(id);
+  
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  
+  useEffect(() => {
+    getResult(id);
+  }, []);
+  
+  if ( !result )
+    return <Text>Loading...</Text>;
+  
   return (
     <View>
       <Text>This is result show screen</Text>
+      <Text>ID: {id}</Text>
     </View>
   );
 };
